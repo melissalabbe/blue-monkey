b7bbaadb2f592a890fd90759a0f7aa9fdeaab186
MELISSA LABBE <melissalabbe@MELISSAs-MacBook-Pro.local>
2015-01-17 11:12:54 -0500

    update build info

--- BlueMonkeyTests/BlueMonkeyTests.swift
+++ BlueMonkeyTests/BlueMonkeyTests.swift
@@ -2,8 +2,8 @@
 //  BlueMonkeyTests.swift
 //  BlueMonkeyTests
 //
-//  Created by MELISSA LABBE on 12/22/14.
-//  Copyright (c) 2014 MELISSA LABBE. All rights reserved.
+//  Created by MELISSA LABBE on 1/15/15.
+//  Copyright (c) 2015 MELISSA LABBE. All rights reserved.
 //
 
 import UIKit
